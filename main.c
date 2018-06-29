2f24341b16cbbd1380822b3b70717f6e0029b7c0
Michelle Adea <mc.adea@gmail.com>
2018-06-28 18:00:25 -0700

    Preprocessor constant variables

--- main.c
+++ main.c
@@ -2,19 +2,18 @@
 */
 
 #include <stdio.h>
+#define TIME 7.5
+#define DISTANCE 425.5
 
 int main() {
-	float distance, time, speed_miph, speed_mps;
+	float speed_miph, speed_mps;
 
-	distance = 425.0;
-	time = 7.5;
-	
-	speed_miph = distance / time;
+	speed_miph = DISTANCE / TIME;
 	speed_mps = speed_miph * 1600.0 / 3600;
 
 	printf("The Speed in Miles per Hour: %f, The Speed in Meters per Second: %f\n", speed_miph, speed_mps);
 	//printf("The Speed in Meters per Second: %f\n", speed_mps);
 
 	getchar();
-	return 0;
+	return 0; 
 }
