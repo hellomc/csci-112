5843962cb5bf59860dce4880ce3e59b631c76d21
Michelle Adea <mc.adea@gmail.com>
2018-06-28 17:54:25 -0700

    Redone in class
    refine later; if there are variables that have static value can use as constant.

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
-#include <stdio.h>
-
-/* Author Michelle Adea
-Date: 2018 June 26
-Purpose: Write a program that converts miles per hour to meters per second.
+/*
 */
 
-int main()
-{
-	float distance;
-	float time;
-	float speed_miph;
-	float speed_mps;
+#include <stdio.h>
 
-	distance = 425.5; // Value for distance traveled by car.
-	time = 7.5; // Value for the number of hours for traveling above distance.
+int main() {
+	float distance, time, speed_miph, speed_mps;
 
-	speed_miph = distance / time; // Expression solves for speed in miles per hour.
-	printf("Speed in Miles per Hour = %f\n", speed_miph);
+	distance = 425.0;
+	time = 7.5;
+	
+	speed_miph = distance / time;
+	speed_mps = speed_miph * 1600.0 / 3600;
 
-	speed_mps = speed_miph * 1600.0 / 3600; // Expression converts miph to meters per second.
-	printf("speed in Meters per Second = %f\n", speed_mps);
+	printf("The Speed in Miles per Hour: %f, The Speed in Meters per Second: %f\n", speed_miph, speed_mps);
+	//printf("The Speed in Meters per Second: %f\n", speed_mps);
 
 	getchar();
 	return 0;
