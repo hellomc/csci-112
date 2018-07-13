cc31d03edac9ca11bae869e0aa696c24708e14da
Michelle Adea <mc.adea@gmail.com>
2018-07-12 19:04:01 -0700

    Practicing creating loops

---
+++ example/loops.c
@@ -0,0 +1,42 @@
+/* Author: Michelle Adea
+Date: 2018 July 12
+Description: Practicing for, while, and do while loops.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	//int i = 1;
+
+	/*while (i < 100)
+	{
+		// More efficient than if loop below, repeats cycle 50 times.
+		//printf("%d\n", i);
+		//i = i + 2;
+
+		if (i % 2)
+			printf("%d\n", i);
+		i = i + 1;
+	}
+	*/
+
+	/*while (i < 100)
+	{
+		if ((i & 2) && (i != 53)) // Skips 53.
+			printf("%d\n", i);
+		i = i + 1;
+	}
+	*/
+
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 2)
+			printf("%d\n", i);
+	}
+
+	getchar();
+	return 0;
+}
